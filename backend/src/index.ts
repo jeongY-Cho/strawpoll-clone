f101aaf58c0b8df0e5f9ff75b2673c70a308893b
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-03 13:06:49 -0400

    naive vote checker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import PollStore from "./PollsStore";
+import cookieParser from "cookie-parser";
+
+require("dotenv").config();
 
 const app = express();
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 const pollStore = new PollStore();
 
@@ -17,13 +21,29 @@ app.get("/:id", async (req, res) => {
   }
 });
 
-app.post("/:id", bodyParser.json(), async (req, res) => {
+const validateVote: express.RequestHandler = (req, res, next) => {
+  if (JSON.stringify(req.signedCookies.status)) {
+    res.status(400).send("already voted");
+  } else {
+    next();
+  }
+};
+
+app.post("/:id", validateVote, bodyParser.json(), async (req, res) => {
   const item = req.body as { inc?: number; dec?: number };
 
   try {
     const success = await pollStore.vote(req.params.id, item.inc!);
 
     if (success) {
+      res.cookie("status", JSON.stringify(item), {
+        path: req.path,
+        // secure: true,
+        httpOnly: true,
+        expires: new Date(2147483647000),
+        signed: true,
+      });
+
       res.send("ok");
     } else {
       res.status(404).send("invalid poll id or choice index");
