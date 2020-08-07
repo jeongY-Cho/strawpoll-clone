a9f10e8be2421c157ab29aad5bd2957ed1bddbd9
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-07 01:25:52 -0400

    change str to date obj

--- src/RedisPollStore.ts
+++ src/RedisPollStore.ts
@@ -207,7 +207,7 @@ export default class Cache {
 
               let retObj: IPollAgg = {
                 prompt: response[0].prompt,
-                createdAt: response[0].createdAt,
+                createdAt: new Date(response[0].createdAt),
                 id,
                 total: response[1].total,
                 choices,
